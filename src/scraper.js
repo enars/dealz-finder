bbfcd7b7e10cf6c909f996ef4bf6016f749c7735
Ejnar Sörensen <ejnar.sorensen@nordnet.se>
2026-04-18 23:47:21 +0200

    fix: try stealth plugin

--- src/scraper.js
+++ src/scraper.js
@@ -1,4 +1,6 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
 
 const FORUM_URL =
   "https://www.sweclockers.com/forum/trad/999559-dagens-fynd-bara-tips-ingen-diskussion-las-forsta-inlagget-forst";
